81756726dc3ff988d2819635987441aa9795374f
Nicolas Pereira <nicopereira2802@gmail.com>
2025-12-28 12:31:05 -0300

    repositories logistica completos

---
+++ logistica/src/main/java/com/example/logistica/repositories/TramoRutaRepository.java
@@ -0,0 +1,11 @@
+package com.example.logistica.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.logistica.models.TramoRuta;
+
+@Repository
+public interface TramoRutaRepository extends JpaRepository<Long,TramoRuta>{
+    
+}
